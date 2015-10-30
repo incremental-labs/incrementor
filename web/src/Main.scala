36e3e6f2de1c80f6537550c5e1562385f7aba23e
Tyler Anton <mtyleranton@gmail.com>
2015-10-30 00:26:16 -0500

    foundation for scalajs with phaser

--- web/src/Main.scala
+++ web/src/Main.scala
@@ -1,28 +1,27 @@
 import scala.scalajs.js
 import scala.scalajs.js.JSApp
+import js.Dynamic.{ global => g }
 import org.scalajs.dom.ext._
 import org.scalajs.dom
 import dom.html
 import scalajs.js.annotation.JSExport
+import phaser._
 import entity._
 
 @JSExport
 object Main {
 
-  val testNode = new TestNode()
-
   @JSExport
-  def main(canvas: html.Canvas): Unit = {
-    initGame(canvas)
+  def main(): Unit = {
+    g.game = new Game(width = dom.window.innerWidth,
+                        height = dom.window.innerHeight,
+                        parent = "",
+                        state = js.Dynamic.literal(preload = preload, create = create))
+
   }
 
-  def initGame(canvas: html.Canvas) {
-    val ctx = canvas.getContext("2d").cast[dom.CanvasRenderingContext2D]
+  val preload = () => g.game.load.image("ideos", "assets/IDEOS.png", false)
 
-    canvas.width = canvas.parentElement.clientWidth
-    canvas.height = canvas.parentElement.clientHeight
-
-    testNode.init(ctx, canvas)
-  }
+  val create = () => g.game.add.sprite(700, 300, "ideos")
 
 }
---
+++ web/src/phaser/Phaser.scala
@@ -0,0 +1,43 @@
+package phaser
+
+import scala.scalajs.js
+import js.annotation._
+import js.|
+import org.scalajs.dom.html
+
+@js.native
+object Phaser extends js.Object {
+  val AUTO: Int = js.native
+  val CANVAS: Int = js.native
+}
+
+@js.native
+@JSName("Phaser.Game")
+class Game(
+  width: Double | String = 800,
+  height: Double | String = 600,
+  renderer: Int = Phaser.AUTO,
+  parent: String | html.Element = "",
+  state: js.Dynamic) extends js.Object {
+
+  val add: GameObjectFactory = js.native
+  val load: Loader = js.native
+}
+
+@js.native
+@JSName("Phaser.Loader")
+class Loader(game: Game) extends js.Object {
+  def image(key: String, url: String, overwrite: Boolean): Loader = js.native
+}
+
+@js.native
+@JSName("Phaser.GameObjectFactory")
+class GameObjectFactory(game: Game) extends js.Object {
+  def sprite(x: Double = 0, y: Double = 0, key: String = js.native): Sprite = js.native
+}
+
+@js.native
+@JSName("Phaser.Sprite")
+class Sprite(game: Game, x: Double = 0, y: Double = 0, key: String, frame: String) extends js.Object {
+
+}
